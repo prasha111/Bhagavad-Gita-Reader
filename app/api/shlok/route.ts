774213212ea42a4dfccca732f9486d9db1c0d7c4
prasha111 <prashantsharma556@gmail.com>
2026-05-03 16:31:31 +0530

    fix

--- app/api/shlok/route.ts
+++ app/api/shlok/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import  {connectDB}  from "../../../lib/db";
+import  {connectDB}  from "@/lib/db";
 import Shlok from "@/models/Shlok";
 
 export async function POST(req: Request) {

93f89b210db8526d359becb8c57db69d5aa6c7e9
prasha111 <prashantredeem@gmail.com>
2026-05-07 15:03:02 +0530

    onclick forward logic

--- components/reader/NavigationControls.tsx
+++ components/reader/NavigationControls.tsx
@@ -21,9 +21,9 @@ export default function NavigationControls({
   return (
     <div className="fixed bottom-6 left-0 right-0 flex justify-between px-6">
       
-      {(shlok !==1 && chapter === 1) && <button
+      { <button
         onClick={() =>
-          router.push(`/reader/${chapter}/${Number(shlok) - 1}`)
+          ((shlok !==1 && chapter === 1) &&router.push(`/reader/${chapter}/${Number(shlok) - 1}`))
         }
         className="bg-white shadow-lg px-4 py-2 rounded-full hover:scale-105 transition text-black"
       >
